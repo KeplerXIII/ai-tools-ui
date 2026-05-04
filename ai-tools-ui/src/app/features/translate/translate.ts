eac96bef81bffb63f592cd3a7db5e9f5eed2a840
Alexandr Prokhorenkov <prokhorenkovcag@ntckompas.org>
2026-05-04 17:09:08 +0200

    Начал делать переводчик

--- ai-tools-ui/src/app/features/translate/translate.ts
+++ ai-tools-ui/src/app/features/translate/translate.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TextareaModule } from 'primeng/textarea';
+import { ChipModule } from 'primeng/chip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,6 +20,9 @@ import { TranslateApi } from './translate-api';
     MatFormFieldModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    FloatLabelModule,
+    TextareaModule,
+    ChipModule,
   ],
   templateUrl: './translate.html',
   styleUrl: './translate.scss',
